ae7319547f55b5de4c655978f4724775ef00f563
lucashfcardoso <lucashfcardoso@gmail.com>
2024-05-28 20:24:29 -0300

    Perfil Blog Pessoal

--- src/paginas/perfil/Perfil.tsx
+++ src/paginas/perfil/Perfil.tsx
@@ -4,31 +4,38 @@ import { AuthContext } from '../../contexts/AuthContext'
 
 function Perfil() {
 
-  let navigate = useNavigate()
+    const navigate = useNavigate()
 
     const { usuario } = useContext(AuthContext)
 
     useEffect(() => {
         if (usuario.token === "") {
             alert('Você precisa estar logado')
-            navigate("/login")
+            navigate("/")
         }
     }, [usuario.token])
-    
-  return (
 
-    <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
+    return (
+        <div className='container mx-auto m-4 rounded-2xl overflow-hidden'>
 
-      <img className='w-full h-72 object-cover border-b-8 border-white' src={''} alt="Capa do Perfil" />
-      <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
+            <img 
+                className='w-full h-72 object-cover border-b-8 border-white' 
+                src="https://i.imgur.com/ZZFAmzo.jpg" alt="Capa do Perfil" />
 
-      <div className="relative mt-[-6rem] h-72 flex flex-col bg-sky-500 text-white text-2xl items-center justify-center">
-        <p>Nome: {usuario.nome} </p>
-        <p>Email: {usuario.usuario}</p>
-      </div>
+            <img 
+                className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' 
+                src={usuario.foto} alt={`Fotoperfil de ${usuario.nome}`} />
 
-    </div>
-  )
+            <div 
+                className="relative mt-[-6rem] h-72 flex flex-col 
+                    bg-sky-500 text-white text-2xl items-center justify-center"
+            >
+                <p>Nome: {usuario.nome} </p>
+                <p>Email: {usuario.usuario}</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default Perfil
