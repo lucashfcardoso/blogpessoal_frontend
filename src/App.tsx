85ac26092862bda2d31a84c410c0c14733ff7775
lucashfcardoso <lucashfcardoso@gmail.com>
2024-05-28 20:21:56 -0300

    Perfil (Blog Pessoal)

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import DeletarTema from './components/temas/deletarTema/DeletarTema'
 import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens'
 import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem'
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem'
+import Perfil from './paginas/perfil/Perfil'
 
   function App() {
     return (
@@ -34,6 +35,7 @@ import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPosta
               <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
               <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
               <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
                 </Routes>
           
              </div>
--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,40 +1,45 @@
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
-import { useContext } from "react"
+import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
 
 function Navbar() {
+  let navigate = useNavigate()
 
-    const navigate = useNavigate()
+    const { usuario, handleLogout } = useContext(AuthContext)
 
-    const { handleLogout } = useContext(AuthContext)
-
-    function logout(){
+    function logout() {
         handleLogout()
-        alert('O usuário foi desconectado com sucesso!')
-        navigate('/')
+        alert('Usuário deslogado com sucesso')
+        navigate('/login')
     }
 
-    return (
-        <>
-            <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
-            
-                <div className="container flex justify-between text-lg">
-                    <Link to="/home" className="text-2xl font-bold">Blog Pessoal</Link>
+    let navbarComponent
 
-                    <div className='flex gap-4'>
-                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
-                        <Link to='/temas' className='hover:underline'>Temas</Link>
-                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-                        Perfil
+    if(usuario.token !== "") {
+      navbarComponent = (
+        <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
-                        {}
-                        <Link to="" onClick={logout} className="hover:underline">Sair</Link>
-                    </div>
-                </div>
+            <div className='flex gap-4'>
+              <Link to='/postagens' className='hover:underline'>Postagens</Link>
+              <Link to='/temas' className='hover:underline'>Temas</Link>
+              <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+              <Link to='/perfil' className='hover:underline'>Perfil</Link>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
             </div>
-        </>
-    )
+            
+          </div>
+        </div>
+      )
+    }
+
+  return (
+    <>
+      {navbarComponent}
+    </>
+  )
 }
 
 export default Navbar
